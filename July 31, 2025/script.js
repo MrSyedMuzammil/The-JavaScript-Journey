a9005e8fe5e04d066ec85f11a10553f9a097567f
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-07-31 13:00:19 +0500

    July 31, 2025 Type Conversion and Type Coercion

--- July 31, 2025/script.js
+++ July 31, 2025/script.js
@@ -9,3 +9,27 @@ if (isOldEnough) {
     `You are young, you need to wait ${yearsLeft} years to apply for driver's license.`
   );
 }
+
+// Type Conversion
+
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Syed"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// Type Coercion
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" / 2);
+
+// Guessing
+
+let n = "1" + 1;
+n = n - 1;
+console.log(n);
+
+// In first step n becomes 11, then 11 -1 = 10
