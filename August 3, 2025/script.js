6dd6bffb3b4e8e4cc762ac88894589568d5fd675
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-03 23:53:07 +0500

    August 3, 2025 Basic Array Operations Add elements to the initial or end of the array remove elements from the initial or end of the array Find the index of the elements Checking specific element inside of the array

--- August 3, 2025/script.js
+++ August 3, 2025/script.js
@@ -48,3 +48,35 @@ console.log(age1);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[2])];
 console.log(ages);
+
+// Add elements to the array
+const allFriends = friends.push("Mabby");
+console.log(friends); // Mabby added at last
+
+friends.unshift("Mabby"); // This one adds element at 0 i.e the first element
+console.log(friends);
+
+// Removing elements
+friends.pop(); // Removes the last element
+console.log(friends);
+
+const popped = friends.pop();
+console.log(popped); // Till here the last two elements are removed
+
+friends.shift(); // This removes the first element from the array
+console.log(friends);
+
+//This tells the index of the element in an array
+console.log(friends.indexOf("Saadi"));
+console.log(friends.indexOf("Subhan"));
+
+friends.push(23);
+console.log(friends.includes("Umer"));
+console.log(friends.includes("Ijaz"));
+console.log(friends.includes("23"));
+
+// Checking through if
+
+if (friends.includes("Umer")) {
+  console.log(`You have a friend called Umer`);
+}
