a4c6125a6f813b317c5275f856d1a65d6491a9e4
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-04 16:13:43 +0500

    August 4, 2025 Object Dot notation and Bracket Notation Practice Make object log object with dot notation and bracket notation Take input from the user and log the required value of object

---
+++ August 4, 2025/script.js
@@ -0,0 +1,34 @@
+const syed = {
+  firstName: "Syed",
+  lastName: "Muzammil",
+  age: 2025 - 2003,
+  job: "Developer",
+  friends: ["Farman bacha", "Naveed Rehman", "Kashif Afridi", "others"],
+};
+
+console.log(syed);
+
+// Using dot Notation to access the objects data
+console.log(syed.lastName);
+console.log(`Syed ${syed.lastName} is ${syed.age} years old`);
+
+const interestedIn = prompt(
+  "What do you want to know about Syed ? Choose between firstName, lastName, age, job and friends"
+);
+
+// This will not work as with dot notation we can not add expression
+// console.log(syed.interestedIn);
+
+// With using the bracket notation we can access, as it take expressions
+// if age is clicked, the age value should go to console and so does for others
+// console.log(syed[interestedIn]);
+
+// if the user inputs some other property that does not exist like location than it shows undefined
+
+if (syed[interestedIn]) {
+  console.log(syed[interestedIn]);
+} else {
+  console.log(
+    `Wrong Request! Please choose Choose between firstName, lastName, age, job and friends `
+  );
+}
