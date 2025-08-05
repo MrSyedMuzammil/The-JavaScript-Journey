893cdbc09bf4c0bd85fe3274f4b3314312aaebb4
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-05 17:38:51 +0500

    August 5, 2025 Playing with Loops in Arrays Add data types of one array elements into another array Add the data of one array into another array Format the data and put into another array

--- August 5, 2025/script.js
+++ August 5, 2025/script.js
@@ -1,10 +1,39 @@
-console.log("Repitition 1");
-console.log("Repitition 2");
-console.log("Repitition 3");
-console.log("Repitition 4");
-console.log("Repitition 5");
+// console.log("Repitition 1");
+// console.log("Repitition 2");
+// console.log("Repitition 3");
+// console.log("Repitition 4");
+// console.log("Repitition 5");
 
 // For loop will work unless the condition is not true
-for (let rep = 0; rep <= 10; rep++) {
-  console.log(`Repitition ${rep}`);
+// for (let rep = 0; rep <= 10; rep++) {
+//   console.log(`Repitition ${rep}`);
+// }
+
+// Looping the elements of an array
+
+const developerProfile = [
+  "Syed",
+  "Muzammil",
+  2025 - 2003,
+  "Developer",
+  ["Farman Bacha", "Naveed Rehman", "Kashif Afridi"],
+];
+
+const profileDataTypes = []; // Here we will add the types of the above syed array through loop
+
+const repeatDataSyedArray = []; // Here we will push all the data from the syed array to this new array
+
+const formattedProfileData = []; // This will contian all the data fromthe syed array but in chronological order
+
+for (let i = 0; i < developerProfile.length; i++) {
+  console.log(developerProfile[i]);
+
+  profileDataTypes.push(typeof developerProfile[i]); // This will keep the types of the syed aray in this new array
+
+  repeatDataSyedArray.push(`${developerProfile[i]} Element : ${i}`); // This will add the data of syed array into this new empty array
+  formattedProfileData.push(`Element ${i} : ${developerProfile[i]}`); // With this and the one below we can add the elements in order to the new array
+  //   formattedProfileData.push("Element " + i + " : " + syed[i]);
 }
+console.log(profileDataTypes);
+console.log(repeatDataSyedArray);
+console.log(formattedProfileData);
