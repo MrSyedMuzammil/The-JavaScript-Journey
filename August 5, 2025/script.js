85a45aadb7db8614a723ce95f54b3167cbc5e78b
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-05 15:48:00 +0500

    August 5, 2025 Basic For Loop for increment by 1

---
+++ August 5, 2025/script.js
@@ -0,0 +1,10 @@
+console.log("Repitition 1");
+console.log("Repitition 2");
+console.log("Repitition 3");
+console.log("Repitition 4");
+console.log("Repitition 5");
+
+// For loop will work unless the condition is not true
+for (let rep = 0; rep <= 10; rep++) {
+  console.log(`Repitition ${rep}`);
+}
