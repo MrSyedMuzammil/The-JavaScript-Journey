746b59ac903af89a7ca508b3ffcb5db45da81502
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-12 16:05:23 +0500

    August 12, 2025 Add examples of JavaScript array methods
    Added code samples and explanations demonstrating various JavaScript array methods, including length, toString, at, join, indexOf, concat, splice, toSpliced, and slice. These examples illustrate how to manipulate and access array elements for educational purposes.

--- August 3, 2025/script.js
+++ August 3, 2025/script.js
@@ -80,3 +80,81 @@ console.log(friends.includes("23"));
 if (friends.includes("Umer")) {
   console.log(`You have a friend called Umer`);
 }
+
+// ARRAY METHODS
+
+const brothers = [
+  "Rahmat Said",
+  "Muhammad Ismail",
+  "Janoo",
+  "Lala G",
+  "Fazle Subhan",
+  "Mahmood",
+  "Syed Muzammil",
+];
+
+// ARRAY LENGTH / NUMER OF ELEMENTS
+console.log(brothers.length);
+
+/*
+// Set the length of an Array
+// For example if we have to set the length of array to 4, it will include the first four brothers
+brothers.length = 4;
+console.log(brothers);
+brothers.length = 7;
+console.log(brothers); // It will not add back the elements we removed before, we can push or unshift them manually.
+*/
+console.log(brothers);
+
+// ARRAY TO STRING
+let myBrothers = brothers.toString();
+console.log(myBrothers);
+
+// GETTING SPECIFIC INDEXED ELEMENT FROM AN ARRAY
+let mymiddleBrother = brothers.at(3); // or let middleBrother = brothers[3];
+console.log(mymiddleBrother);
+
+// ARRAY JOIN ( == toString() but we can use a separator)
+brothers.join(", ");
+console.log(brothers); // these are usually used for html to show array elements as strings
+
+// CHANGING ELEMENTS FROM A SPECIFIC INDEX
+console.log(brothers.indexOf("Janoo"));
+brothers[2] = "Sabz Ali";
+console.log(brothers);
+
+// CONCATENATION OF ARRAYS
+const myTrusty = brothers.concat(friends);
+console.log(myTrusty);
+// concat() can also concatenate a string or any number of arrays
+const specials = myTrusty.concat("Wo", "Wo");
+console.log(specials);
+
+// SPLICING ARRAYS (Splicing adds elements to an array)
+specials.splice(specials.length - 3, specials.length - 1, "Ye", "Ye"); // array.splice(startIndex, deleteCount, item1, item2, ...)
+// If we want just them to be added to specific index, we should keep the deleteCount 0, it will not delete any element
+// specials.splice(specials.length - 3, 0, "Ye", "Ye");
+console.log(specials);
+
+// JAVASCRIPT ARRAY toSpliced()
+/*
+  The difference between the new toSpliced() method and the old splice() method is that the new method creates a new array, keeping the original array unchanged, while the old method altered the original array.
+*/
+
+const splicedSpecials = specials.toSpliced(
+  specials.length - 1,
+  0,
+  "Woohoo",
+  "Wohoo"
+);
+console.log(splicedSpecials);
+console.log(specials);
+
+// JAVASCRIPT ARRAY slice()
+// The slice() method slices out a piece of an array into a new array:
+myTrusty.slice(specials.length - 2, specials.length - 1);
+console.log(myTrusty);
+
+// slicing the myTrusty array and removing all the friends except the first two and others too
+const outsidersTrusty = myTrusty.slice(7); // This will slice out at index i.e 7 brothers at beginning
+console.log(outsidersTrusty);
